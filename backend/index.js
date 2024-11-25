23261429d85b376f5f80646baced0b1ff3f72989
Sandegr <sander.grimstad00@gmail.com>
2024-11-25 11:33:01 +0100

    Fixed wrong backed url

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const multer = require("multer");
 const { Storage } = require("@google-cloud/storage");
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 // Google Cloud Storage configuration
 const storage = new Storage({ projectId: "idata2502-cloudproject" });
-const bucketName = 'file-sharing-service'
+const bucketName = "file-sharing-service";
 const bucket = storage.bucket(bucketName);
 
 const upload = multer({ storage: multer.memoryStorage() });
@@ -18,23 +16,23 @@ const upload = multer({ storage: multer.memoryStorage() });
 app.post("/upload", upload.single("file"), async (req, res) => {
   try {
     if (!req.file) {
-      return res.status(400).json({error: 'No file uploaded'});
+      return res.status(400).json({ error: "No file uploaded" });
     }
-    
+
     const blob = bucket.file(req.file.originalname);
     const blobStream = blob.createWriteStream({
       resumable: false,
-      contentType: req.file.originalname,
-      predefinedAcl: 'publicRead',
+      contentType: req.file.mimetype,
+      predefinedAcl: "publicRead",
     });
 
     blobStream.on("error", (err) => res.status(500).send({ error: err.message }));
 
     blobStream.on("finish", () => {
       const publicUrl = `https://storage.googleapis.com/${bucketName}/${blob.name}`;
-      res.status(200).json({ message: 'File uploaded successfully', url: publicUrl});
+      res.status(200).json({ message: "File uploaded successfully", url: publicUrl });
     });
-      
+
     blobStream.end(req.file.buffer);
   } catch (error) {
     res.status(500).send({ error: error.message });
@@ -46,8 +44,8 @@ app.get("/files", async (req, res) => {
   try {
     const [files] = await bucket.getFiles();
     const fileData = files.map((file) => ({
-      name: file.name,
-      url: `https://storage.googleapis.com/${bucketName}/${file.name}`
+      name: file.name, // File name matches the key in your bucket
+      url: `https://storage.googleapis.com/${bucketName}/${file.name}`, // Generate public URL
     }));
     res.status(200).json(fileData);
   } catch (error) {
